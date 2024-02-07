db03e44a081eeead3a842ff310d2aaa69ecc2933
TusharBaldaniya <108427398+TusharBaldaniya@users.noreply.github.com>
2024-02-07 16:29:38 +0530

    Update sw.js

--- Public/sw.js
+++ Public/sw.js
@@ -5,3 +5,23 @@ self.addEventListener('push', event => {
     body: 'Yay it works!',
   });
 });
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('your-app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/gyanmanjari',  // Adjust this according to your app's starting point
+        // Add other static assets like CSS, JavaScript, images, etc.
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
